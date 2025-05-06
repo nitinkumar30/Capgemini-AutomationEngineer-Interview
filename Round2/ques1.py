f33c1ebc4d823e276463a7bc69379944dc8bd975
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2025-05-06 22:00:11 +0530

    Delete Round2 directory

--- Round2/ques1.py
+++
@@ -1,7 +0,0 @@
-D={1:"a",7:"b",3:"e",11:"z",21:"o"}
-# print the sum of all the keys whose value is a vowel in the English alphabet.
-
-rslt = sum(key for key, value in D.items() if value in {'a', 'e', 'i', 'o', 'u'})
-print(rslt)
-
-# Interviewer asked me to code a 1 liner for this
